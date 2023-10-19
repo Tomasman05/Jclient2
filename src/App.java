2ada19d272b525b6002b96050616298c09d51b8e
Vito <lolozol80xd@gmail.com>
2023-10-19 09:57:27 +0200

    update

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ import controllers.TodoController;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        new TodoController().index();
+        new TodoController().create();
     }
 }
--- src/controllers/TodoController.java
+++ src/controllers/TodoController.java
@@ -12,24 +12,28 @@ public class TodoController {
     Client client;
 
     public TodoController() {
-        client = new Client();
-        prop = new Prop().getProp();
+        this.client = new Client();
+        this.prop = new Prop().getProp();
         this.host = this.prop.getProperty("host");
         this.endpoint = this.prop.getProperty("endpoint");
     }
 
     public void index() {
         String url = this.host + this.endpoint;
-        String body = "{ \"userId\": 1,"+"\"title\":\"Tavesz baktale!!!\","+"\"completed\": true"+" }";
-        String res = client.post(url, body );
-        String full = client.get(url);
+        String res = client.get(url);
         System.out.println(res);
-        System.out.println(full);
     }
 
     public void create() {
-
-    }
+        String url = this.host + this.endpoint;
+        String body = "{ \"userId\": 1,"+
+        "\"title\":\"Telefon ajándékéba másnak\","+
+        "\"completed\": false"+
+        " }";
+        System.out.println(body);
+        String res = client.post(url, body);
+        System.out.println(res);  
+    } 
 
     public void update() {
 
